822de569c1177aab733598d8e65634dbda5244c2
Jeremy Price <jeremy.price@member.sage-au.org.au>
2003-05-27 10:19:24 +0000

    Import of JEPRICE/Net-MDNS-Server-0.01 from CPAN.
    gitpan-cpan-distribution: Net-MDNS-Server
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         JEPRICE/Net-MDNS-Server-0.01.tar.gz
    gitpan-cpan-author:       JEPRICE
    gitpan-cpan-maturity:     released

---
+++ example.pl
@@ -0,0 +1,12 @@
+use lib './blib';
+use lib './blib/arch/';
+use lib './blib/lib/';
+use Net::MDNS::Server ':all';
+print "Loaded module\nFinding IP address\n";
+my $ifc = join "", `ifconfig`;
+$ifc =~ m/addr:(\d+\.\d+\.\d+\.\d+)/s;
+my $addr = $1;
+claim_hostname("wiggles", "10.11.11.11");
+#service("myhost", $addr, 444, "perl", "tcp");
+print "Loaded service\n";
+while (1) {process_network_events}
